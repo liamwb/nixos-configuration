147df6b4ae758ce0c514f31814f67225f75bbae9
liamwb <liam.woodbaker@gmail.com>
2024-09-25 12:25:37 +1000

    made a snippet for inline math

--- home/nvim/plugins/custom-snippets/tex.lua
+++ home/nvim/plugins/custom-snippets/tex.lua
@@ -4,29 +4,36 @@ local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("tex", {
-	s({ trig = "((", snippetType = "snippet" }, {
+	s({ trig = "mm", snippetType = "snippet" }, {
 		t({ "\\( " }),
 		i(1),
 		t({ " \\)" }),
 	}),
-	s({ trig = "[", snippetType = "snippet" }, {
-		t({ "[" }),
-		i(1),
-		t({ "]" }),
-	}),
-	s({ trig = "{", snippetType = "snippet" }, {
-		t({ "{" }),
-		i(1),
-		t({ "}" }),
-	}),
-	s({ trig = "(", snippetType = "snippet" }, {
-		t({ "(" }),
-		i(1),
-		t({ ")" }),
-	}),
-	s({ trig = "|", snippetType = "snippet" }, {
-		t({ "|" }),
-		i(1),
-		t({ "|" }),
-	}),
+
+	-- old snippets from when I had auto-pairs disabled
+	-- s({ trig = "((", snippetType = "snippet" }, {
+	-- 	t({ "\\( " }),
+	-- 	i(1),
+	-- 	t({ " \\)" }),
+	-- }),
+	-- s({ trig = "[", snippetType = "snippet" }, {
+	-- 	t({ "[" }),
+	-- 	i(1),
+	-- 	t({ "]" }),
+	-- }),
+	-- s({ trig = "{", snippetType = "snippet" }, {
+	-- 	t({ "{" }),
+	-- 	i(1),
+	-- 	t({ "}" }),
+	-- }),
+	-- s({ trig = "(", snippetType = "snippet" }, {
+	-- 	t({ "(" }),
+	-- 	i(1),
+	-- 	t({ ")" }),
+	-- }),
+	-- s({ trig = "|", snippetType = "snippet" }, {
+	-- 	t({ "|" }),
+	-- 	i(1),
+	-- 	t({ "|" }),
+	-- }),
 })
