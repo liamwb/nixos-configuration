30f1ef58ab555d26d1df666a1a6c6861dbe57148
liamwb <liam.woodbaker@gmail.com>
2024-09-27 16:00:16 +1000

    got proton working

--- hosts/liam-pc-nixos/mount-drives.nix
+++ hosts/liam-pc-nixos/mount-drives.nix
@@ -3,7 +3,13 @@
 
   fileSystems."/var/lib/nvme" = {
     device = "/dev/nvme0n1p2";
-    fsType = "ntfs-3g";
-    options = [ "rw" ];
+    fsType = "ntfs";
+    options = [ "rw" "users" "exec" "umask=000 0 0" ];
   };
+
+  # To use proton with games stored on an NTFS filesystem, follow the instructions https://github.com/ValveSoftware/Proton/wiki/Using-a-NTFS-disk-with-Linux-and-Windows
+
+  # in particular, you need to symlink the compatdata directory on the ntfs drive, because ntfs is crap:
+  #   $ mkdir -p ~/.steam/steam/steamapps/compatdata
+  #   $ ln -s ~/.steam/steam/steamapps/compatdata /path/to/gamedisk/Steam/steamapps/
 }
