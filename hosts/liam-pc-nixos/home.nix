739741d4a9dd2eb17396315c16f5bd71d12058b9
liamwb <liam.woodbaker@gmail.com>
2025-06-16 09:42:01 +1000

    monitor layout

--- hosts/liam-pc-nixos/home.nix
+++ hosts/liam-pc-nixos/home.nix
@@ -11,8 +11,8 @@
   
   wayland.windowManager.hyprland.settings = {
     monitor = [
-    "DP-3, 1920x1080@165, 0x0, 1"
-    "DP-2, 1920x1080@60, 1920x0, 1"
+    "DP-1, 1920x1080@60, 0x0, 1"
+    "DP-3, 1920x1080@165, 1920x0, 1"
     ];
 
     input = {
