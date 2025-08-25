bdab8f5f0972768e4776104ff8a0ad65665e0443
liamwb <liam.woodbaker@gmail.com>
2025-08-25 10:48:47 +1000

    fuzzel but it doesn't work yet

---
+++ home/fuzzel/default.nix
@@ -0,0 +1,16 @@
+{config, lib, pkgs, ...}:
+let 
+  theme = config.lib.stylix.colors.withHashtag;
+  background = theme.base00;  # Default Background
+  light_background = theme.base01;  # Lighter Background (Used for status bars, line number and folding marks)
+  selection_background = theme.base02;  # Selection Background
+  foreground = theme.base05;  # Default Foreground, Caret, Delimiters, Operators
+in
+{
+    programs.fuzzel = {
+        enable = true;
+        settings = {
+            colors.background = background;
+        };
+    };
+}
