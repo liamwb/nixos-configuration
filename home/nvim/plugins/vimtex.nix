f5b2468698bb2b57d8821533fa1ef0610d13d507
liamwb <liam.woodbaker@gmail.com>
2025-11-18 17:38:58 +1100

    fancy vimtex symbols

--- home/nvim/plugins/vimtex.nix
+++ home/nvim/plugins/vimtex.nix
@@ -16,6 +16,22 @@
         indent_on_ampersands = 0;  # can't believe anyone wants this
 
         view_method = "zathura";
+
+        syntax_conceal = {
+          accents = 0;
+          cites = 1;
+          fancy = 1;
+          greek = 1;
+          ligatures = 1;
+          math_delimiters = 1;
+          math_super_sub = 0;
+          math_symbols = 1;
+          spacing = 0;
+          styles = 1;
+          math_bounds = 0;
+          math_fracs = 0;
+          sections = 0;
+        };
       };
     };
 
