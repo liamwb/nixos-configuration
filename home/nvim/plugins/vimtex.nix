0179e3d0a96ae9f4c344f71d05a23f7b991ba249
liamwb <liam.woodbaker@gmail.com>
2026-02-23 10:45:06 +1100

    nvim conceal options

--- home/nvim/plugins/vimtex.nix
+++ home/nvim/plugins/vimtex.nix
@@ -1,23 +1,25 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
   programs.nixvim = {
 
     # for syntax_conceal
-    opts = { conceallevel = 2; }; 
+    opts = {
+      conceallevel = 2;
+    };
 
     plugins.vimtex = {
       enable = true;
 
       texlivePackage = pkgs.texlive.combined.scheme-full;
       settings = {
-        matchparen_enabled = 0;  # vimtex built in pathenthesis matchin is very slow
+        matchparen_enabled = 0; # vimtex built in pathenthesis matchin is very slow
 
-        delim_timeout = 10;  # set the timeout for fancy bracket matching to 100ms
+        delim_timeout = 10; # set the timeout for fancy bracket matching to 100ms
         delim_stopline = 50;
 
-        quickfix_mode = 0;  # don't open the quick fix menu by default
+        quickfix_mode = 0; # don't open the quick fix menu by default
 
-        indent_on_ampersands = 0;  # can't believe anyone wants this
+        indent_on_ampersands = 0; # can't believe anyone wants this
 
         view_method = "zathura";
 
@@ -27,11 +29,12 @@
           fancy = 1;
           greek = 1;
           ligatures = 1;
+          spacing = 0;
+          styles = 0;
+          texTabularChar = 0;
           math_delimiters = 1;
           math_super_sub = 0;
           math_symbols = 1;
-          spacing = 0;
-          styles = 1;
           math_bounds = 0;
           math_fracs = 0;
           sections = 0;
